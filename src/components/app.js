93acab77e88a8f97fcc558457049f8e2f7a0e1ef
Dr3wbit <eightbitandrew@gmail.com>
2018-04-30 18:38:15 -0700

    completed react input forms

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,36 @@
+import React, { Component } from 'react';
+import Contactlist from './contact_list'
+import ContactForm from './contact_form'
+import contactData from '../data/contact'
 
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="text-center">Address Book</h1>
+                <div className="row">
+                    <div className="col-4">
+                        <ContactForm add={this.addContact}/>
+                    </div>
+                    <Contactlist contacts={this.state.contacts}/>
+                </div>
+            </div>
+        );
+    }
+    constructor (props){
+        super(props);
+        this.addContact = this.addContact.bind(this);
+        this.state = {
+            contacts: contactData,
+        }
+    }
+    addContact(contact){
+        this.setState({
+            contacts: [contact,...this.state.contacts]
+        });
+        console.log('App: addComponent:', contact );
+    }
+}
+
+export default App;
 
